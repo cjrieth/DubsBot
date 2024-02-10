2ed5d2a44fa0eec4b5f33e5f7425e3b75f814060
cj.rieth@att.net <cj.rieth@att.net>
2024-02-10 04:32:10 +0000

    refactoring

--- scripts/scrapecatalog.sh
+++ scripts/scrapecatalog.sh
@@ -2,6 +2,6 @@
 
 echo Downloading Course Catalog for all Majors ...
 
-lynx -listonly -nonumbers -dump https://www.washington.edu/students/crscat/ | grep .html | grep crscat | grep -v \# | grep -v glossary | sort -u | xargs -l1 wget -q -P /workspaces/azure-search-openai-demo/data
+lynx -listonly -nonumbers -dump https://www.washington.edu/students/crscat/ | grep .html | grep crscat | grep -v \# | grep -v glossary | sort -u | xargs -l1 wget -q -P /workspaces/DubsBot/data
 
 echo Done!
--- scripts/scrapeschedules.sh
+++ scripts/scrapeschedules.sh
@@ -5,6 +5,6 @@ read limit
 
 echo Downloading Time Schedules for all Majors ...
 
-lynx -listonly -nonumbers -dump https://www.washington.edu/students/timeschd/SPR2024/ | grep .html | grep SPR2024 | grep -v \# |sort -u | head -n $limit | xargs -l1 wget -q -P /workspaces/azure-search-openai-demo/data
+lynx -listonly -nonumbers -dump https://www.washington.edu/students/timeschd/SPR2024/ | grep .html | grep SPR2024 | grep -v \# |sort -u | head -n $limit | xargs -l1 wget -q -P /workspaces/DubsBot/data
 
 echo Done!
